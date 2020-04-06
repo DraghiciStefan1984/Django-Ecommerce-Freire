5559c29add0a9a75f1cb47a84d8abf62d2f70e8c
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-04-06 08:51:29 +0300

    added working layout with links

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Item, Order, OrderItem
+
 
 # Create your views here.
+def item_list(request):
+    context={'items':Item.objects.all()}
+    return render(request, 'home-page.html', context)
+
+
+def home(request):
+    return render(request, 'home-page.html', {})
+
+
+def checkout(request):
+    return render(request, 'checkout-page.html', {})
+
+
+def products(request):
+    return render(request, 'product-page.html', {})
